1fa1e0192cbdeb7f62cc2329e3d13a65916cdbd2
thiyagarajan <thiyagarajannv@yahoo.com>
2012-05-31 21:07:35 +0530

    fixes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   acts_as_nested_set
-  attr_accessible :name, :parent_id, :lft, :rgt
+  attr_accessible :name, :parent_id#, :lft, :rgt
   # attr_accessible :title, :body
   has_many :victories
   def path_name
